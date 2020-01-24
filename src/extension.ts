0d199fea2ae5abd6b8888107571263f00ac5446f
Ivan Towlson <itowlson@microsoft.com>
2020-01-24 14:27:28 +1300

    Extension boilerplate

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+        vscode.window.showInformationMessage('Hello World!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() {
+}
