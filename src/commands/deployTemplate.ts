fd51ba689c3c4c81f21e4a04441286e3ac09334d
Ivan Towlson <itowlson@microsoft.com>
2020-01-30 10:39:35 +1300

    Less terrible CRD names

--- src/commands/deployTemplate.ts
+++ src/commands/deployTemplate.ts
@@ -19,7 +19,7 @@ export async function deployTemplate(textEditor: vscode.TextEditor, _edit: vscod
         return;
     }
 
-    const templateName = path.basename(regoDoc.uri.fsPath, '.rego');
+    const templateName = path.basename(regoDoc.uri.fsPath, '.rego');  // TODO: or the package name?
     const rego = regoDoc.getText();
 
     const template = constraintTemplate(templateName, rego, associatedSchemaObj);
@@ -48,12 +48,14 @@ function constraintTemplate(templateName: string, rego: string, schema: JSONSche
 }
 
 function constraintTemplateCRDSpec(templateName: string, schema: JSONSchema): any {
+    const kind = kindify(templateName);
+    const identifier = identifierfy(templateName);
     return {
         names: {
-            kind: templateName + 'Kind',
-            listKind: templateName + 'KindList',
-            plural: templateName + 'Plural',
-            singular: templateName
+            kind: kind,
+            listKind: kind + 'List',
+            plural: identifier,
+            singular: identifier
         },
         validation: {
             openAPIV3Schema: schema
@@ -66,4 +68,26 @@ function constraintTemplateTarget(rego: string): any {
         target: 'admission.k8s.gatekeeper.sh',
         rego: rego
     };
+}
+
+function kindify(name: string): string {
+    if (!name || name.length === 0) {
+        return '';
+    }
+
+    const bits = name.split(/[^a-zA-Z0-9]/);
+    const titleCasedBits = bits.map((s) => titleCase(s));
+    const kindified = titleCasedBits.join('');
+    return kindified;
+}
+
+function titleCase(s: string): string {
+    if (s === '') {
+        return s;
+    }
+    return s[0].toUpperCase() + s.slice(1);
+}
+
+function identifierfy(name: string): string {
+    return name.replace(/^a-zA-Z0-9/g, '').toLowerCase();
 }
