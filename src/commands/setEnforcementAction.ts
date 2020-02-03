5b6cf22415398607c7dc9ef44989513d3c399c2f
itowlson <ivan.towlson@microsoft.com>
2020-02-03 16:59:48 +1300

    Command to change the enforcement action

--- src/commands/setEnforcementAction.ts
+++ src/commands/setEnforcementAction.ts
@@ -1,9 +1,14 @@
 import * as vscode from 'vscode';
 import * as k8s from 'vscode-kubernetes-tools-api';
+import * as yaml from 'js-yaml';
 import { unavailableMessage, longRunning } from '../utils/host';
 import { ResourceBrowser } from '../ui/resource-browser';
-import { getConstraint, ConstraintDetail, ConstraintStatusDetail, ConstraintViolation } from '../gatekeeper';
-import { failed } from '../utils/errorable';
+import { withTempFile } from '../utils/tempfile';
+
+const ENFORCEMENT_ACTIONS = [
+    { baseLabel: 'Deny', action: 'deny'},
+    { baseLabel: 'Dry Run', action: 'dryrun'},
+] as const;
 
 export async function setEnforcementAction(target: any) {
     const clusterExplorer = await k8s.extension.clusterExplorer.v1;
@@ -26,13 +31,42 @@ export async function setEnforcementAction(target: any) {
 }
 
 async function trySetEnforcementAction(kubectl: k8s.KubectlV1, templateName: string, constraintName: string): Promise<void> {
-    const constraint = await longRunning(`Getting constraint ${templateName}/${constraintName}`, () =>
-        getConstraint(kubectl, templateName, constraintName)
+    const sr = await longRunning(`Getting constraint ${templateName}/${constraintName}`, () =>
+        kubectl.invokeCommand(`get ${templateName}/${constraintName} -o yaml`)
     );
-    if (failed(constraint)) {
-        await vscode.window.showErrorMessage(`Can't get constraint info: ${constraint.error[0]}`);
+    if (!sr || sr.code !== 0) {
+        const message = sr ? sr.stderr : 'Unable to run kubectl';
+        await vscode.window.showErrorMessage(`Can't get constraint info: ${message}`);
         return;
     }
 
-    vscode.window.showInformationMessage('pretending to prompt for the enforcement action');
+    const constraintYAML = sr.stdout;
+    const constraint = yaml.safeLoad(constraintYAML);
+    const currentAction: string = ((constraint.spec || {}).enforcementAction) || 'deny';
+
+    const availableActions = ENFORCEMENT_ACTIONS.map((a) => {
+        const suffix = (a.action === currentAction) ? ' (current)' : '';
+        return { label: a.baseLabel + suffix, ...a };
+    });
+    const selection = await vscode.window.showQuickPick(availableActions);
+    if (!selection || selection.action === currentAction) {
+        return;
+    }
+
+    constraint.spec.enforcementAction = selection.action;
+    const newContraintYAML = yaml.safeDump(constraint);
+
+    const applyResult = await withTempFile(newContraintYAML, 'yaml', (filename) =>
+        longRunning(`Updating constraint ${templateName}/${constraintName} with new action`, () =>
+            kubectl.invokeCommand(`apply -f ${filename}`)
+        )
+    );
+
+    if (!applyResult || applyResult.code !== 0) {
+        const message = applyResult ? applyResult.stderr : 'Unable to run kubectl';
+        await vscode.window.showErrorMessage(`Error updating constraint ${templateName}/${constraintName}: ${message}`);
+        return;
+    }
+
+    await vscode.window.showInformationMessage(`Updated constraint ${templateName}/${constraintName} with new action`);
 }
