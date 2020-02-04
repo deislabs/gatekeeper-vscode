f2f9a9598b0eccf0bde1766cc06edac0a9bc1b22
Ivan Towlson <itowlson@microsoft.com>
2020-02-04 15:09:29 +1300

    Infer starter schema

--- src/authoring/associations.ts
+++ src/authoring/associations.ts
@@ -34,6 +34,7 @@ export function associatedSchemaPath(document: vscode.TextDocument): string {
 }
 
 export interface JSONSchema {
+    readonly $schema?: string;
     readonly type?: string;
     readonly properties?: { [name: string]: JSONSchema };
     readonly items?: JSONSchema;
