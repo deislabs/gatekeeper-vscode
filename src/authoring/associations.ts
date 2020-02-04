1c63fde43b230c07b8ae630afa66666643d41309
Ivan Towlson <itowlson@microsoft.com>
2020-02-04 14:29:22 +1300

    Open existing parameters schema

--- src/authoring/associations.ts
+++ src/authoring/associations.ts
@@ -23,7 +23,7 @@ export async function associatedSchemaDocument(document: vscode.TextDocument): P
         const schemaDocument = await vscode.workspace.openTextDocument(schemaPath);
         return schemaDocument;
     } catch {
-        // It throws if the schema document doesn't exist or isn't parseable - we can just abandon ship in these cases
+        // It throws if the schema document doesn't exist - we can just abandon ship in this case
         return null;
     }
 }
--- src/commands/parametersSchema.ts
+++ src/commands/parametersSchema.ts
@@ -1,5 +1,14 @@
 import * as vscode from 'vscode';
+import { associatedSchemaDocument } from '../authoring/associations';
 
 export async function parametersSchema(textEditor: vscode.TextEditor, _edit: vscode.TextEditorEdit) {
-    await vscode.window.showInformationMessage('parameters schema');
+    const regoDoc = textEditor.document;
+    const associatedSchemaDoc = await associatedSchemaDocument(regoDoc);
+
+    if (associatedSchemaDoc) {
+        await vscode.window.showTextDocument(associatedSchemaDoc);
+        return;
+    }
+
+    vscode.window.showWarningMessage("You haven't done creating new ones yet Ivan!");
 }
